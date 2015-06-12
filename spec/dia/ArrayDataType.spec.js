14c981281de0523f9b30143d8e7127a31232e9b1
remvst <remvst@gmail.com>
2015-06-12 03:28:35 +0200

    More tests

--- spec/dia/ArrayDataType.spec.js
+++ spec/dia/ArrayDataType.spec.js
@@ -5,6 +5,66 @@ describe('an array data type', function(){
 		
 		expect(complex.containedType).toBe(simple);
 	});
+	
+	it('does validation correctly', function(){
+		var simple = new dia.DataType({
+			validate: function(value){
+				return value === 'rem';
+			}
+		});
+		var complex = new dia.ArrayDataType(simple);
+		
+		expect(complex.validate([])).toBe(true);
+		expect(complex.validate(['rem'])).toBe(true);
+		expect(complex.validate(['rem', 'rem'])).toBe(true);
+		expect(complex.validate(['rem', 1, 'rem'])).toBe(true);
+		expect(complex.validate([2, 1])).toBe(true);
+	});
+	
+	it('does JSON imports/exports correctly', function(){
+		var simple = new dia.DataType({
+			toJSON: function(value){
+				return 'json';
+			},
+			fromJSON: function(value){
+				return 'data';
+			}
+		});
+		var complex = new dia.ArrayDataType(simple);
+		
+		expect(complex.toJSON([1])).toEqual(['json']);
+		expect(complex.fromJSON([1])).toEqual(['data']);
+	});
+	
+	it('does HTML conversions correctly', function(){
+		var simple = new dia.DataType({
+			toHTML: function(value){
+				var input = document.createElement('span');
+				input.innerHTML = value;
+				return input;
+			},
+			fromHTML: function(html){
+				return html.innerHTML;
+			}
+		});
+		var complex = new dia.ArrayDataType(simple);
+		
+		var value = ['a', 'b', 'c', 'd'];
+		var input = complex.createHTMLInput(value);
+		
+		var spans = input.querySelectorAll('span');
+		expect(spans.length).toEqual(value.length);
+		
+		for(var i = 0 ; i < spans.length ; i++){
+			expect(spans[i].innerHTML).toEqual(value[i]);
+		}
+		
+		for(var i = 0 ; i < spans.length ; i++){
+			spans[i].innerHTML = 'z';
+		}
+		
+		expect(complex.getValueFromHTMLInput(input)).toEqual(['z', 'z', 'z', 'z']);
+	});
 });
 
 describe('built in string array data type', function(){
