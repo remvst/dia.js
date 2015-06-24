212b357260f3d9b891c6f3442c863bb1135c6b3d
remvst <remvst@gmail.com>
2015-06-25 00:47:34 +0200

    Snapping when moving a relation

--- src/dia/MoveRelationDragHandle.js
+++ src/dia/MoveRelationDragHandle.js
@@ -6,14 +6,24 @@ dia.MoveRelationDragHandle = function(element, area, property){
 
 extend(dia.MoveRelationDragHandle, dia.DragHandle);
 
+dia.MoveRelationDragHandle.prototype.dragStart = function(x, y){
+	this.accumDX = 0;
+	this.accumDY = 0;
+	
+	this.initialPoints = this.element.getProperty(this.property);
+};
+
 dia.MoveRelationDragHandle.prototype.dragMove = function(dx, dy){
+	this.accumDX += dx;
+	this.accumDY += dy;
+	
 	var points = this.element.getProperty(this.property);
 	
 	var newPoints = [];
 	for(var i = 0 ; i < points.length ; i++){
 		newPoints.push({
-			x: points[i].x + dx,
-			y: points[i].y + dy
+			x: dia.snap(this.initialPoints[i].x + this.accumDX, this.element.sheet.gridSize),
+			y: dia.snap(this.initialPoints[i].y + this.accumDY, this.element.sheet.gridSize)
 		});
 	}
 	
