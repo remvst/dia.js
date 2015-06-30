1ed239ebfd281824f3efe53ed3a5ed83ebc42c27
remvst <remvst@gmail.com>
2015-07-01 01:11:28 +0200

    Added cardinalities

--- dist/dia.js
+++ dist/dia.js
@@ -1592,7 +1592,7 @@ dia.Area.defineIntersection('line', 'rectangle', function(line, rectangle){
 
 dia.BrokenLineArea = function(options){
 	dia.Area.call(this);
-
+	
 	this.getPoints = options.points;
 	this.thickness = options.thickness || 10;
 	this.type = 'brokenline'
@@ -1606,7 +1606,7 @@ dia.BrokenLineArea.prototype.contains = function(x, y){
 
 dia.BrokenLineArea.prototype.indexOfLineThatContains = function(x, y){
 	var points = this.getPoints(),
-		area,
+		area, 
 		minDistance,
 		dist,
 		closest = -1;
@@ -1618,7 +1618,7 @@ dia.BrokenLineArea.prototype.indexOfLineThatContains = function(x, y){
 			y2: function(){ return points[i + 1].y; },
 			thickness: this.thickness
 		});
-
+		
 		if(area.contains(x, y)){
 			dist = area.distance(x, y);
 			if(closest === -1 || dist < minDistance){
@@ -1627,7 +1627,7 @@ dia.BrokenLineArea.prototype.indexOfLineThatContains = function(x, y){
 			}
 		}
 	}
-
+	
 	return closest;
 };
 
@@ -1635,7 +1635,7 @@ dia.BrokenLineArea.prototype.render = function(c){
 	c.strokeStyle = 'red';
 	c.lineWidth = this.thickness / 4;
 	c.beginPath();
-
+	
 	var points = this.getPoints();
 	for(var i = 0 ; i < points.length ; i++){
 		c.lineTo(points[i].x, points[i].y);
@@ -1661,30 +1661,30 @@ dia.BrokenLineArea.prototype.getLength = function(){
 
 dia.BrokenLineArea.prototype.getPositionAtRatio = function(ratio){
 	ratio = dia.limit(ratio, 0, 1);
-
+	
 	var totalLength = this.getLength(),
 		expectedLength = totalLength * ratio,
 		points = this.getPoints(),
 		length = 0,
 		nextLength;
-
+	
 	for(var i = 0 ; i < points.length - 1 ; i++){
 		nextLength = length + dia.distance(
 			points[i].x, points[i].y,
 			points[i + 1].x, points[i + 1].y
 		);
-
+		
 		if(expectedLength >= length && expectedLength <= nextLength){
 			break;
 		}else{
 			length = nextLength;
 		}
 	}
-
+	
 	var segmentLength = nextLength - length,
 		distanceLeft = expectedLength - length,
 		segmentRatio = distanceLeft / segmentLength;
-
+	
 	// i = point before
 	// i + 1 = point after
 	return {
@@ -1704,12 +1704,12 @@ dia.Area.defineIntersection('line', 'brokenline', function(line, brokenLine){
 			y2: function(){ return points[i + 1].y; },
 			thickness: brokenLine.thickness
 		});
-
+		
 		if(area.intersectsWith(line)){
 			return true;
 		}
 	}
-
+	
 	return false;
 });
 
@@ -1724,12 +1724,12 @@ dia.Area.defineIntersection('rectangle', 'brokenline', function(rectangle, broke
 			y2: function(){ return points[i + 1].y; },
 			thickness: brokenLine.thickness
 		});
-
+		
 		if(area.intersectsWith(rectangle)){
 			return true;
 		}
 	}
-
+	
 	return false;
 });
 
@@ -3429,6 +3429,18 @@ dia.uml.COMPOSITION = dia.generic.RELATION.clone({
 	label: 'Composition relation'
 });
 
+dia.uml.COMPOSITION.addProperty(new dia.Property({
+	id: 'cardinalityFrom',
+	type: dia.DataType.STRING,
+	default: '*'
+}));
+
+dia.uml.COMPOSITION.addProperty(new dia.Property({
+	id: 'cardinalityTo',
+	type: dia.DataType.STRING,
+	default: '*'
+}));
+
 dia.uml.COMPOSITION.extendRepresentationFactory(function(element, repr){
 	repr.getPoints = function(){
 		return [repr.fromPosition()].concat(element.getProperty('points')).concat([repr.toPosition()]);
@@ -3441,11 +3453,15 @@ dia.uml.COMPOSITION.extendRepresentationFactory(function(element, repr){
 
 		var p1 = points[points.length - 1];
 		var p2 = points[points.length - 2];
+		var p3 = points[1];
+		var p4 = points[0];
 
-		var angle = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+		var angleTo = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+		var angleFrom = Math.atan2(p4.y - p3.y, p4.x - p3.x);
 
+		c.save();
 		c.translate(p1.x, p1.y);
-		c.rotate(angle);
+		c.rotate(angleTo);
 
 		c.fillStyle = '#000';
 		c.beginPath();
@@ -3454,6 +3470,32 @@ dia.uml.COMPOSITION.extendRepresentationFactory(function(element, repr){
 		c.lineTo(40, 0);
 		c.lineTo(20, -10);
 		c.fill();
+
+		c.restore();
+
+		var fromLabelAngle = angleFrom + Math.PI / 2;
+		var fromLabelPosition = {
+			x: p4.x - Math.cos(angleFrom) * 20 + Math.cos(fromLabelAngle) * 20,
+			y: p4.y - Math.sin(angleFrom) * 20 +  Math.sin(fromLabelAngle) * 20
+		};
+
+		c.fillStyle = '#000';
+		c.font = '14pt Arial';
+		c.textAlign = 'center';
+		c.textBaseline = 'middle';
+		c.fillText(element.getProperty('cardinalityFrom'), fromLabelPosition.x, fromLabelPosition.y);
+
+		var toLabelAngle = angleTo + Math.PI / 2;
+		var toLabelPosition = {
+			x: p1.x + Math.cos(angleTo) * 20 + Math.cos(toLabelAngle) * 20,
+			y: p1.y + Math.sin(angleTo) * 20 +  Math.sin(toLabelAngle) * 20
+		};
+
+		c.fillStyle = '#000';
+		c.font = '14pt Arial';
+		c.textAlign = 'center';
+		c.textBaseline = 'middle';
+		c.fillText(element.getProperty('cardinalityTo'), toLabelPosition.x, toLabelPosition.y);
 	}));
 });
 
