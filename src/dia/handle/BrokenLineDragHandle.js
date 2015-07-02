a292135e302c3b2498839d51a617ecdac9f7bb17
remvst <remvst@gmail.com>
2015-07-03 01:11:53 +0200

    Added ability to fuse with first and last points of a broken line

--- src/dia/handle/BrokenLineDragHandle.js
+++ src/dia/handle/BrokenLineDragHandle.js
@@ -62,10 +62,38 @@ dia.BrokenLineDragHandle.prototype.dragDrop = function(x, y){
 	var point1 = points[modifiedIndex - 1];
 	var point2 = points[modifiedIndex + 1];
 
-	var newPoints;
+	var newPoints,
+		needsRemove = false;
 	if(point1 && dia.distance(this.modifiedPoint.x, this.modifiedPoint.y, point1.x, point1.y) <= 10
 	   || point2 && dia.distance(this.modifiedPoint.x, this.modifiedPoint.y, point2.x, point2.y) <= 10){
-		var newPoints = points.slice(0);
+
+		needsRemove = true;
+	}else{
+		// Fusing with the first or last point (not intermediate)
+		var areaPoints = this.area.getPoints();
+		var areaFirstPoint = areaPoints[0];
+		var areaLastPoint = areaPoints[areaPoints.length - 1];
+
+		var areaPoints = this.area.getPoints(),
+			areaFusedPoint;
+		if(modifiedIndex === 0){
+			areaPoints = this.area.getPoints();
+			areaFusedPoint = areaPoints[0];
+			if(dia.distance(this.modifiedPoint.x, this.modifiedPoint.y, areaFusedPoint.x, areaFusedPoint.y) <= 10){
+				needsRemove = true;
+			}
+		}
+		if(modifiedIndex === points.length - 1){
+			areaPoints = this.area.getPoints();
+			areaFusedPoint = areaPoints[areaPoints.length - 1];
+			if(dia.distance(this.modifiedPoint.x, this.modifiedPoint.y, areaFusedPoint.x, areaFusedPoint.y) <= 10){
+				needsRemove = true;
+			}
+		}
+	}
+
+	if(needsRemove){
+		newPoints = points.slice(0);
 		newPoints.splice(modifiedIndex, 1);
 
 		this.element.setProperty(this.property, newPoints);
