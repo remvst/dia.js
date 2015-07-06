ef09a15414fca2dfc68c8a78506f3b60ac6018b1
remvst <remvst@gmail.com>
2015-07-06 09:57:24 +0200

    Updated app

--- dist/dia.js
+++ dist/dia.js
@@ -1966,17 +1966,18 @@ dia.Canvas.prototype.render = function(ctx){
 
 dia.App = function(){
 	dia.EventDispatcher.call(this);
-	
+
 	this.toolbox = new dia.Toolbox();
-	
+
 	this.toolbox.addTool(new dia.SelectionTool());
-	
-	for(var i in dia.ElementType.types){
-		if(dia.ElementType.types[i].creatorTool){
-			this.toolbox.addTool(dia.ElementType.types[i].creatorTool);
+
+	var types = dia.ElementType.types;
+	for(var id in types){
+		for(var k = 0 ; k < types[id].tools.length ; k++){
+			this.toolbox.addTool(types[id].tools[k]);
 		}
 	}
-	
+
 	this.sheet = null;
 };
 
@@ -1988,7 +1989,7 @@ dia.App.prototype.newSheet = function(){
 
 dia.App.prototype.openSheet = function(sheet){
 	this.sheet = sheet;
-	
+
 	this.dispatch('newsheet', {
 		sheet: this.sheet
 	});
--- spec/dia/ui/App.spec.js
+++ spec/dia/ui/App.spec.js
@@ -1,49 +1,49 @@
 describe('an app', function(){
 	it('is initialized properly', function(){
 		var app = new dia.App();
-		
+
 		expect(app.toolbox).not.toBe(null);
 		expect(app.sheet).toBe(null);
 	});
-	
+
 	it('can start', function(){
 		$('<div></div>').attr('id', 'toolbox').appendTo('body');
 		$('<canvas></canvas>').attr('id', 'canvas').appendTo('body');
-		
+
 		var uuid4 = dia.uuid4;
-		
+
 		dia.uuid4 = function(){
 			return 'uuid';
 		};
-		
-		var type1 = new dia.ElementType({
+
+		var type = new dia.ElementType({
 			id: 'type1234'
 		});
-		type1.creatorTool = new dia.CreateTool();
-		
+		type.addTool(new dia.CreateTool());
+
 		var app = new dia.App();
 		app.newSheet();
-		
-		expect(app.toolbox.toolList.indexOf(type1.creatorTool)).not.toEqual(-1);
+
+		expect(app.toolbox.toolList.indexOf(type.tools[0])).not.toEqual(-1);
 		expect(app.sheet).not.toBe(null);
-		
+
 		dia.uuid4 = uuid4;
-		
+
 		$('#toolbox').remove();
 		$('#canvas').remove();
 	});
-	
+
 	it('can open a new sheet', function(){
 		var app = new dia.App();
 		var sheet = new dia.Sheet();
-		
+
 		var event;
 		app.listen('newsheet', function(e){
 			event = e;
 		});
-		
+
 		app.openSheet(sheet);
-		
+
 		expect(app.sheet).toBe(sheet);
 		expect(event.sheet).toBe(sheet);
 	});
